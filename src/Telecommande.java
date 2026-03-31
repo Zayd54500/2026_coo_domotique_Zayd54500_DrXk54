ad64d7f5d1377de2515daea713f96a7179ae31d0
Andaloussi212 <zayd.benz.pro@proton.me>
2026-03-31 11:15:05 +0200

    squelette class telecommande

---
+++ src/Telecommande.java
@@ -0,0 +1,22 @@
+public class Telecommande {
+    Telecommande() {
+
+    }
+
+    void ajouterLampe(Lampe lampe) throws new Error("Code non écrit"){
+    }
+
+    void activerLampe(int indiceLampe) throws new Error("Code non écrit"){
+
+    }
+
+    void desactiverLampe(int indiceLampe) throws new Error("Code non écrit"){
+
+    }
+    void activer() throws new Error("Code non écrite"){
+
+    }
+    String toString() throws new Error("Code non écrite"){
+        
+    }
+}
