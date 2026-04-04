d158b9f3e4c65323fb59947671f4595dec45bdf9
Andaloussi212 <zayd.benz.pro@proton.me>
2026-04-04 18:56:41 +0200

    ajout gestion hifi telecommande

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -4,19 +4,35 @@ public class Telecommande {
 
     private ArrayList<Lampe> lampes;
 
+    private ArrayList<Hifi> hifis;
+
     public Telecommande() {
         lampes = new ArrayList<>();
+        hifis = new ArrayList<>();
     }
 
     public void ajouterLampe(Lampe l) {
         lampes.add(l);
     }
 
+    public void ajouterHifi(Hifi h) {
+        hifis.add(h);
+    }
+
+
+
     public void activerLampe(int indiceLampe) {
         if (indiceLampe < 0 || indiceLampe >= lampes.size()) {
             throw new Error("lampe innexistante")
         }
         lampes.get(indiceLampe).allumer();
+    }
+
+    public void activerHifi(int indiceHifi) {
+        if (indiceHifi < 0 || indiceHifi >= hifis.siez()) {
+            throw new Error("hifi innexistante");
+        }
+        hifis.get(indiceHifi).allumer();
     }
 
     public void desactiverLampe(int indiceLampe) {
@@ -26,20 +42,36 @@ public class Telecommande {
         lampes.get(indiceLampe).eteindre();
     }
 
+    public void desactiverHifi(int indiceHifi) {
+        if (indiceHifi < 0 || indiceHifi >= hifis.siez()) {
+            throw new Error("hifi innexistante");
+        }
+        hifis.get(indiceHifi).eteindre();
+    }
+
     public void activerTout() {
         public void activerTout() {
             for (Lampe l : lampes) {
                 l.allumer();
             }
+            for (Hifi h : hifis) {
+                h.allumer();
         }
 
     }
 
     public String toString() {
-        String resultat = "";
+        String resultat = "Lampes :\n";
         for (int i = 0; i < lampes.size(); i++) {
             resultat += i + " - " + lampes.get(i).toString() + "\n";
         }
+        resultat += "Hifis :\n";
+
+        for (int i = 0; i < hifis.size(); i++) {
+            resultat += i + " - " + hifis.get(i).toString() + "\n";
+        }
+
         return resultat;
+
     }
 }
